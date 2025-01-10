ff63b402fcfcef1e8d4d5aa1ae1b4dfc28fe32b7
jctaoo <jctaoo@outlook.com>
2025-01-10 14:47:02 +0800

    fix dart warnings

--- lib/components/player/player_sheet.dart
+++ lib/components/player/player_sheet.dart
@@ -1,11 +1,8 @@
-import 'package:dart_music_api/music_api.dart';
 import 'package:flt_template/services/music/music_providers.dart';
 import 'package:flt_template/services/music/notifiers/player_notifier.dart';
-import 'package:flt_template/utils/common/logger.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:just_audio/just_audio.dart';
 import 'package:styled_widget/styled_widget.dart';
 
 class PlayerSheet extends HookConsumerWidget {
